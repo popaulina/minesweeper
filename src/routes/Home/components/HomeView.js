f977b82368f5f03c26c8bab1d647af4e4a8451c0
Paulina Levit <levit@nyu.edu>
2017-06-20 21:00:40 -0400

    drawn board

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -2,13 +2,24 @@ import React from 'react'
 import './HomeView.scss'
 import { connect } from 'react-redux'
 
-export const HomeView = (props) => {
-debugger;
-return (
+export const HomeView = (props) => (
   <div>
-
+  	<Board board={props.state.minesweeper.board} />
   </div>
-)}
+)
+
+const Board = ({ board }) => (
+	<div>
+		{ board.map(x => x.map(y => <BoardPiece piece={y} />)) }
+	</div>
+)
+
+const BoardPiece = ({ piece }) => {
+	var styles = { marginLeft: (piece.x * 40), marginTop: (piece.y * 40) }
+	return (
+		<div className={piece.open ? "piece opened" : "piece unopened"} style={styles}> </div>
+	)
+}
 
 const mapStateToProps = (state) => ({ state })
 
